9d82cc7ffe7c623b5520143a555e46c5a631e8e3
jonstiven2011 <jonstiven2011@hotmail.com>
2016-12-02 02:30:58 -0500

    proyecto

---
+++ src/app/model/final.model.ts
@@ -0,0 +1,11 @@
+export class Curso
+{
+    id:number;
+    nombre:string;
+    apellido:string;
+    cedula:number;
+    correo:string;
+    naturaleza:string;
+    prueba:string;
+    valor:number;
+}
