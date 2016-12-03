264c1b44b38886e8c77d1822a4151061fab4106d
jonstiven2011 <jonstiven2011@hotmail.com>
2016-12-03 00:13:27 -0500

    proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,22 @@ import { profinal } from './model/final.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  pruebasList:Array<profinal> =new Array<profinal>();
+
+  constructor(private http:Http){
+
+  }
+
+  listarPruebas(){
+    this.http
+    .get("http://stiven-web2.net16.net/Electiva2ProFinal/test.php")
+    .subscribe((response)=>{
+      this.pruebasList = response.json();
+      console.log(this.pruebasList);
+      let modalPruebas = document.getElementById("modal_listado_pruebas");
+      modalPruebas.style.visibility = "visible";
+    });
+  }
   
 }
